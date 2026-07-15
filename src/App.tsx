d74bc78e0295be74edb24026dcc2b1910f130451
valdesdiegoobed <valdesdiegoobed@gmail.com>
2026-07-15 03:28:20 -0600

    Proteger la aplicación con Firebase Authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { AppBar, Box, Button, Container, CssBaseline, Toolbar, Typography } from '@mui/material';
 import FolderSharedIcon from '@mui/icons-material/FolderShared';
+import { AppBar, Box, Button, Container, CssBaseline, Toolbar, Typography } from '@mui/material';
 import { NavLink, Outlet } from 'react-router-dom';
+import { AuthGate } from './AuthGate';
 
 export function App() {
-  return <><CssBaseline /><AppBar position="sticky" color="primary"><Toolbar><FolderSharedIcon sx={{mr:1}}/><Typography variant="h6" sx={{flexGrow:1}}>Control de Expedientes</Typography><Button color="inherit" component={NavLink} to="/expedientes">Expedientes</Button><Button color="inherit" component={NavLink} to="/configuracion">Firebase</Button></Toolbar></AppBar><Box component="main" sx={{py:3}}><Container maxWidth="lg"><Outlet /></Container></Box></>;
+  return <><CssBaseline /><AuthGate><AppBar position="sticky" color="primary"><Toolbar><FolderSharedIcon sx={{ mr: 1 }}/><Typography variant="h6" sx={{ flexGrow: 1 }}>Control de Expedientes</Typography><Button color="inherit" component={NavLink} to="/expedientes">Expedientes</Button><Button color="inherit" component={NavLink} to="/configuracion">Firebase</Button></Toolbar></AppBar><Box component="main" sx={{ py: 3 }}><Container maxWidth="lg"><Outlet /></Container></Box></AuthGate></>;
 }
