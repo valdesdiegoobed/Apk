58912cdd154e0548b7c2c9ff1c5178e0e23f28c0
valdesdiegoobed <valdesdiegoobed@gmail.com>
2026-07-15 14:41:55 -0600

    Actualizar caché para nueva interfaz

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='expedientes-local-v13';
+const CACHE='expedientes-local-v14';
 const APP_SHELL=['/Apk/','/Apk/manifest.webmanifest','/Apk/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
